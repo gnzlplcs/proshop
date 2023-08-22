7a028bc25a7fdc64c49fc66c81d48e4c4a6d75f1
gnzlplcs <palaciosgm@outlook.com>
2023-08-22 02:50:37 -0500

    :construction: Create components
    Header.jsx | Footer.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
-    <h1>Welcome to PropShop</h1>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          Welcome to ProShop
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
