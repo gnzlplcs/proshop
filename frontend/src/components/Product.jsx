f3bbee6039bc862ba2ffb3d50dd5d14118c6ec67
gnzlplcs <palaciosgm@outlook.com>
2023-08-29 04:28:47 -0500

    :construction: Create Rating component

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
@@ -16,7 +17,11 @@ const Product = ({ product }) => {
           </Card.Title>
         </Link>
 
-        <Card.Text as="h3">${product.size}</Card.Text>
+        <Card.Text as="div">
+          <Rating value={ product.rating} text={`${product.numReviews} reviews`}/>
+        </Card.Text>
+
+        <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
   );
